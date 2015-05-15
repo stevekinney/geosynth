74023cbb29f34ef24a7294eda8eb7feb0574e26e
Steve Kinney <hello@stevekinney.net>
2015-05-15 00:25:57 -0600

    Set up project

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const webpack = require("webpack");
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.scss$/, loader: "style!css!sass" }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss']
+  }
+};
